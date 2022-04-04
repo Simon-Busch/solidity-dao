6075f3589f54b974aae3ec2a48fe73a86bcc29fc
simonbusch <Simon.busch@live.fr>
2022-04-04 08:59:20 +0200

    finish test on model || proposal creationé

--- test/index.ts
+++ test/index.ts
@@ -50,21 +50,74 @@ describe("Tests for the Project DAO ---------", function () {
     });
   });
 
-  describe("Creating a proposal", function () {
-    it("Should be restricted to the stakeholder", async function () {
+  describe("Proposals ----", function () {
+    let proposalList;
+    beforeEach(async () => {
+      await projectDaoContract.createProposal(
+        "make me king",
+        "0x8887d92b863ACc546cd7372B4627de39548F85c4",
+        20
+      );
+      proposalList = await projectDaoContract.getProposals();
+    });
+    it("Creation should be restricted to the stakeholder", async function () {
       const isStake: boolean = await projectDaoContract
         .connect(owner[1])
         .isStakeholder();
       expect(isStake).to.be.false;
     });
-    it("should be able to create a proposal", async function () {
-      await projectDaoContract.createProposal(
-        "make me king",
-        "0x8887d92b863ACc546cd7372B4627de39548F85c4",
-        500
-      );
-      const proposalList = await projectDaoContract.getProposals();
+    it("Should be able to create a proposal", async function () {
       expect(proposalList.length).to.be.greaterThan(0);
+    });
+    it("Should have an id of 0", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.id.toNumber()).to.be.equal(0);
+    });
+    it("Should have an requested amount of votes equal to 20", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.amount.toNumber()).to.be.equal(20);
+    });
+    it("Should have a live period of minimum one week", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.livePeriod.toNumber() / 60 / 60 / 7).to.be.greaterThan(
+        24
+      );
+    });
+    it("Should have an initial about of votes -for- of 0", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.votesFor.toNumber()).to.be.equal(0);
+    });
+    it("Should have an initial about of votes -again- of 0", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.votesAgainst.toNumber()).to.be.equal(0);
+    });
+    it("Should have a description", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.description).to.be.equal("make me king");
+    });
+    it("Should not be accepted yet", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.votingPassed).to.be.false;
+    });
+    it("Should not be paid yet", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.paid).to.be.false;
+    });
+    it("Should have an address for the project", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.projectAddress).to.be.equal(
+        "0x8887d92b863ACc546cd7372B4627de39548F85c4"
+      );
+    });
+    it("Should have a proposer", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.proposer).to.be.equal(owner[0].address);
+    });
+    it("Should have a zero address for paidBy", async function () {
+      const firstProp = await projectDaoContract.getProposal(0);
+      expect(firstProp.paidBy).to.be.equal(
+        "0x0000000000000000000000000000000000000000"
+      );
     });
   });
 });
