c2859b398778b8bbec5d56291b4680d3fe6cec5e
simonbusch <Simon.busch@live.fr>
2022-04-05 11:14:24 +0200

    identation

--- test/index.ts
+++ test/index.ts
@@ -202,11 +202,10 @@ describe("Tests for the Project DAO ---------", function () {
 
   describe("Fallback receive -----", function () {
     it("should be able to send a contribution", async function () {
-
-    await owner[0].sendTransaction({
-			to: projectDaoContract.address,
-			value: ethers.utils.parseEther("1.0"),
-		});
+      await owner[0].sendTransaction({
+        to: projectDaoContract.address,
+        value: ethers.utils.parseEther("1.0"),
+      });
 
       const balance = await projectDaoContract.getBalance();
       // 1000000000000000000 wei = 1 eth
